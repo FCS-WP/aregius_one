03e54735838583b47754fb829896fb2ac52b1b23
taiphan-fcs <tai.phan@floatingcube.com>
2025-03-12 18:09:21 +0700

    update changes

--- src/wp-content/themes/zippy-elementor-child/assets/js/app.js
+++ src/wp-content/themes/zippy-elementor-child/assets/js/app.js
@@ -12,5 +12,33 @@ let Main = {
 };
 
 
-console.log($('.more-link'));
+// console.log($('.more-link'));
 // Main.init();
+
+
+let maxHeight = 0;
+$('#section-review .lakit-testimonials__item .lakit-testimonials__content .lakit-testimonials__comment').each(function() {
+    $(this).css('height', 'auto');
+    let height = $(this).outerHeight(); 
+    maxHeight = Math.max(maxHeight, height);
+});
+
+
+$('#section-review .lakit-testimonials__item .lakit-testimonials__content .lakit-testimonials__comment').css('height', maxHeight + 'px');
+
+
+// change product image on variation selection
+
+$("body").on("change", "#single-product .variations select", function(e){
+  let _value = $(this).val();
+  $(product_variations).each(function(index, variation) {
+    let variation_name = variation.name;
+    if (variation_name.indexOf(_value) > -1) {
+      let feature_image = variation.image;
+      $("#single-product .elementor-widget-image img").attr({
+        src: feature_image,
+        srcset: feature_image
+      })
+    }
+  })
+})
--- src/wp-content/themes/zippy-elementor-child/includes/zippy_custom.php
+++ src/wp-content/themes/zippy-elementor-child/includes/zippy_custom.php
@@ -33,3 +33,70 @@ function exclude_addon_from_related_products( $related_posts, $product_id, $args
     return $related_posts;
 }
 add_filter( 'woocommerce_related_products', 'exclude_addon_from_related_products', 10, 3 );
+
+
+
+
+
+
+function get_elementor_product_variations() {
+
+    $product_id = get_the_ID();
+
+    $product = wc_get_product($product_id);
+    
+    if (!$product) {
+        return '<p>Không tìm thấy sản phẩm.</p>';
+    }
+    
+
+    if ($product->is_type('variable')) {
+        $output = '';
+        $variations = $product->get_available_variations();
+        $prices = [];
+
+        foreach ($variations as $variation) {
+
+            $attributes = $variation['attributes'];
+            $variation_name = [];
+            foreach ($attributes as $key => $value) {
+                $attribute_label = wc_attribute_label(str_replace('attribute_', '', $key));
+                $variation_name[] = $attribute_label . ': ' . $value;
+            }
+            $variation_name = implode(', ', $variation_name);
+            
+
+            $variation_image = $variation['image']['url'] ?? '';
+
+            // get price
+            $variation_price = $variation['display_price'];
+            if (empty($variation_price)) {
+                $variation_price = $variation['display_regular_price'];
+            }
+
+            $prices[] = floatval($variation_price);
+
+            $variations_array[] = [
+                'name' => $variation_name,
+                'image' => $variation_image,
+                'price' => $variation_price
+            ];
+        }
+        
+        $min_price = min($prices);
+        $max_price = max($prices);
+        if ($min_price === $max_price) {
+            $price_range = wc_price($min_price);
+        } else {
+            $price_range = wc_price($min_price) . ' - ' . wc_price($max_price);
+        }
+        echo $price_range;
+        $output = '<script type="text/javascript">';
+        $output .= 'var product_variations = ' . json_encode($variations_array) . ';';
+        $output .= '</script>';
+        return $output;
+    }
+}
+
+
+add_shortcode('product_variations', 'get_elementor_product_variations');
