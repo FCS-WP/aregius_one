c162f0df4d560ba42029341844c8d01f68f77b7f
admin@luna.com <admin@luna.com>
2025-03-12 11:47:13 +0700

    add plugin

--- src/wp-content/themes/zippy-elementor-child/includes/zippy_custom.php
+++ src/wp-content/themes/zippy-elementor-child/includes/zippy_custom.php
@@ -9,3 +9,27 @@ function shin_scripts()
 
     wp_enqueue_script('main-scripts-js', THEME_URL . '-child' . '/assets/dist/js/main.min.js', array('jquery'), $version, true);
 }
+
+
+// Remove product in Add-on Category from Related Product
+function exclude_addon_from_related_products( $related_posts, $product_id, $args ) {
+    
+    $addon_category = get_term_by( 'slug', 'add-on', 'product_cat' );
+    
+    if ( $addon_category && !empty( $related_posts ) ) {
+        
+        $filtered_related = array();
+        
+        foreach ( $related_posts as $related_id ) {
+            
+            if ( !has_term( $addon_category->term_id, 'product_cat', $related_id ) ) {
+                $filtered_related[] = $related_id;
+            }
+        }
+        
+        return $filtered_related;
+    }
+    
+    return $related_posts;
+}
+add_filter( 'woocommerce_related_products', 'exclude_addon_from_related_products', 10, 3 );
